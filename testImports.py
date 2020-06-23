edbd72346c43b15c5fea23ba25634f99ffd2b4d3
Ben Carey <Ben Carey>
2020-06-24 00:39:34 +0100

    still working on that testImports thing

--- testImports.py
+++ testImports.py
@@ -25,13 +25,13 @@ proj_dir = os.path.abspath(relative_path_to_proj_dir)
 
 for subdir, dirs, files in os.walk(proj_dir):
     for filename in files:
-        print ""
+        print "---------------------"
         filepath = os.path.join(subdir, filename)
         if filepath.endswith(".js") or filepath.endswith(".mjs"):
-            file = open(filepath, "r")
-            contents = file.read()
+            js_file = open(filepath, "r")
+            contents = js_file.read()
             # PATTERN: import {...} from '...js'
-            import_pattern = r"import.*\{.*\}.*from.*'.*js'"
+            import_pattern = r"import.*?\{.*?\}.*?from.*?'.*?js'"
             imports = re.findall(
                 import_pattern,
                 contents,
@@ -39,11 +39,13 @@ for subdir, dirs, files in os.walk(proj_dir):
             )
             for match in imports:
                 print match
+                
                 # PATTERN: ...'(...)'
                 file_pattern = r".*'(.*)'"
                 relative_path_to_import = re.match(
                     file_pattern,
                     match,
+                    flags=re.DOTALL,
                 ).group(1)
                 path_to_import = os.path.normpath(
                     os.path.join(
@@ -52,6 +54,65 @@ for subdir, dirs, files in os.walk(proj_dir):
                     )
                 )
                 print path_to_import
+
+                # PATTERN ...{(...)}
+                objects_pattern = r".*\{(.*)\}"
+                objects_list = re.match(
+                    objects_pattern,
+                    match,
+                    re.DOTALL,
+                ).group(1)
+                print objects_list
+                objects = re.split(
+                    r"[,\s]+",
+                    objects_list,
+                )
+                # remove empty strings
+                objects = [
+                    object for object in objects if object
+                ]
+                print objects
+
+                import_file = open(path_to_import, "r")
+                import_contents = import_file.read()
+
+                """
+                Will need to refactor to turn this into a function to
+                allow recursive searching AND
+                want to build up a database of each file we've FINISHED
+                searching through saying 'this is what this file exports'
+                so if we go back to a file that's already been searched
+                we can just check if the object is in the database.
+
+                def search_imports(objects, file, database={}):
+                    open file.
+                    for each object:
+                        if import file already searched:
+                            if object not in import_file_database:
+                                faulty_imports.append([filepath, object])
+                        else:
+                            search for 'export (var/class/function/"") object'
+                            if found:
+                                if match is of form 'export...{...object...} from newfile':
+                                    search_imports(object)
+                                else:
+                                    done (/)
+                            else:
+                                search 'export * from...':
+                                for each found:
+                                    search file it's from
+
+                def search_exports(file, database={}):
+                    open file
+                    search for 'export (var/class/function/""/*) <word>'
+                    for each match:
+                        if match is "export {...} from 'file'":
+                            search_exports(file, database)
+                        else if match 
+
+                """
+
+                print ""
 
 
             #break
