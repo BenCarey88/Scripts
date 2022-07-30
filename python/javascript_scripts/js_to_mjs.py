0856001dbb27b96316902a140463030b9abc7ecb
Ben Carey <bencarey88@gmail.com>
2022-07-30 18:42:05 +0100

    [dirzip] - added dir zip command

---
+++ python/javascript_scripts/js_to_mjs.py
@@ -0,0 +1,37 @@
+
+import argparse
+import os
+
+
+def process_command_line():
+    """Setup command line parser and return directory to check.
+
+    Returns:
+        (argparse.Namespace): arguments from commandline.
+    """
+    parser = argparse.ArgumentParser(
+        description="Test relative imports in javascript project."
+    )
+    parser.add_argument(
+        "directory",
+        metavar="<directory>",
+        default=".",
+        type=str,
+        nargs='?', # 0 or 1
+        help="path to project directory"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    directory = process_command_line().directory
+    for subdir, dirs, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(subdir, filename)
+            if filepath.endswith(".js"):
+                new_filepath = os.path.join(
+                    subdir,
+                    filename.rstrip("js") + "mjs"
+                )
+                print ("Renaming ", filepath, " to ", new_filepath, "\n\n")
+                os.rename(filepath, new_filepath)
---
+++ python/python_scripts/remove_pycache.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+current_directory = os.getcwd()
+
+for subdir, dirs, files in os.walk(current_directory):
+    for dirname in dirs:
+        if dirname == "__pycache__":
+            path = os.path.join(subdir, dirname)
+            print ("Removing directory {0}".format(path))
+            shutil.rmtree(path)
+    for filename in files:
+        if filename.endswith(".pyc"):
+            path = os.path.join(subdir, filename)
+            print ("Removing file {0}".format(path))
+            os.remove(path)
